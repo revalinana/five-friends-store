4b12298386ecca70600c26c943eebe3e88c7b9a0
revalinana <revalinaacute@gmail.com>
2024-09-24 15:37:05 +0700

    add: Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <main className="overflow-x-hidden antialiased text-neutral-800">
       <Navbar />
+      <Hero />
     </main>
   )
 }
